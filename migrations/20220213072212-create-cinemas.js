bd34b3493a2c67eff577730504890bf3ed843c5a
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-07 17:54:38 +0700

    Update 20220213072212-create-cinemas.js

--- migrations/20220213072212-create-cinemas.js
+++ migrations/20220213072212-create-cinemas.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Cinemas', {
+    await queryInterface.createTable('cinemas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -39,6 +39,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cinemas');
+    await queryInterface.dropTable('cinemas');
   }
 };
