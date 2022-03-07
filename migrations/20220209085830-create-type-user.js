eacc292cf3fe549db8d7bda09871a7480ff4031b
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-07 17:53:02 +0700

    Update 20220209085830-create-type-user.js

--- migrations/20220209085830-create-type-user.js
+++ migrations/20220209085830-create-type-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('TypeUsers', {
+    await queryInterface.createTable('typeusers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('TypeUsers');
+    await queryInterface.dropTable('typeusers');
   }
 };
