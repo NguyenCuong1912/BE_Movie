3663f705978072e7eacfbe030e25f783a71404ea
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-07 17:54:16 +0700

    Update 20220213072054-create-group-cinemas.js

--- migrations/20220213072054-create-group-cinemas.js
+++ migrations/20220213072054-create-group-cinemas.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('GroupCinemas', {
+    await queryInterface.createTable('groupcinemas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('GroupCinemas');
+    await queryInterface.dropTable('groupcinemas');
   }
 };
