ca6d0800bb2f8c52bc1f7fa60a2a82837840fa31
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-08 10:46:09 +0700

    Update 20220209132644-create-users.js

--- migrations/20220209132644-create-users.js
+++ migrations/20220209132644-create-users.js
@@ -36,7 +36,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'TypeUsers',
+          model: 'typeusers',
           key: 'id'
         }
       },
