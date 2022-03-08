ee170202c874b155ad5503282d052a75bc21a642
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-08 10:47:25 +0700

    Update 20220213073136-create-rooms.js

--- migrations/20220213073136-create-rooms.js
+++ migrations/20220213073136-create-rooms.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Cinemas',
+          model: 'cinemas',
           key: 'id'
         }
       },
