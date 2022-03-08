1c430af4daf1ec54f038f810e77d709fb3fcbec5
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-08 08:01:10 +0700

    Update 20220215002304-create-tickets.js

--- migrations/20220215002304-create-tickets.js
+++ migrations/20220215002304-create-tickets.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('tickets', {
+    await queryInterface.createTable('Tickets', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -38,6 +38,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('tickets');
+    await queryInterface.dropTable('Tickets');
   }
 };
