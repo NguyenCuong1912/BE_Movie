e8584da0a6942c6e630df061851d13c851f17076
NguyenCuong1912 <84374576+NguyenCuong1912@users.noreply.github.com>
2022-03-08 10:46:58 +0700

    Update 20220213072212-create-cinemas.js

--- migrations/20220213072212-create-cinemas.js
+++ migrations/20220213072212-create-cinemas.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'GroupCinemas',
+          model: 'groupcinemas',
           key: 'id'
         }
       },
